e6253b31474f5f3c07049c9ecf4f90446bc0dd56
abinakolin <jboubato@gmail.com>
2018-01-17 17:41:59 -0600

    third commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'nm3xy%%q0qg%^nx22-1wsg@9h@g&p^20%ga_4xtb3j@6u@jp_z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'abinakolin.pythonanywhere.com',
